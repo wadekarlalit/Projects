5e4d7f78b398360e4d06ea4ea3ad3bd9427335c1
wadekarlalit <lalitwadekr@gmail.com>
2026-02-06 00:56:27 +0530

    start working on logic according to LLD and DSA consepts

--- The-Logic-Calculator/index.js
+++ The-Logic-Calculator/index.js
@@ -1,1 +1,12 @@
 console.log("JS connected successfully");
+
+const currentVal = 0;
+const preVal = 0;
+const history = [];
+let numbers = document.getElementById("btn");
+
+numbers.addEventListener("click", add)
+
+function add(e){
+    console.log("hello", typeof(e.target.value))
+}
